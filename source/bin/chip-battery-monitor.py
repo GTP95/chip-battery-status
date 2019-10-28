4c43b1d7179d88590bccfe7e9c9556029064d120
Giacomo Tommaso Petrucci <giacomo.tom.petrucci@tiscali.it>
2019-10-28 15:19:13 +0100

    Added basic logging

--- source/bin/chip-battery-monitor.py
+++ source/bin/chip-battery-monitor.py
@@ -1,13 +1,18 @@
 #!/bin/python
 
 import subprocess
+import logging
+import os
 
 cmd="systemctl poweroff"	#specifies the command to run on low battery
 
 def main():
+	os.makedirs("/var/log/chip-battery-monitor", exist_ok=True)
+	logging.basicConfig(filename='/var/log/chip-battery-monitor', level=logging.INFO, format='%(asctime)s %(message)s')
 	percentageFile=open("/usr/local/lib/chip-battery-status/percentage", "r")
 	isChargingFile=open("/usr/local/lib/chip-battery-status/charging", "r")
 	percentage=percentageFile.read()
 	isCharging=isChargingFile.read()
 	if percentage<=5 && isCharging==0:
+		logging.info('I think battery is at %i\% so I'm shutting down C.H.I.P.', percentage)
 		subprocess.call(cmd, shell=True)
