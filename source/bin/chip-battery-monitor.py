ff69b1b80bc6d949440dbbea268111571ff8fcf6
Giacomo Tommaso Petrucci <giacomo.tom.petrucci@tiscali.it>
2019-10-28 15:26:10 +0100

    Changed log's date format to little endian: middle endian makes no sense!

--- source/bin/chip-battery-monitor.py
+++ source/bin/chip-battery-monitor.py
@@ -8,7 +8,7 @@ cmd="systemctl poweroff"	#specifies the command to run on low battery
 
 def main():
 	os.makedirs("/var/log/chip-battery-monitor", exist_ok=True)
-	logging.basicConfig(filename='/var/log/chip-battery-monitor', level=logging.INFO, format='%(asctime)s %(message)s')
+	logging.basicConfig(filename='/var/log/chip-battery-monitor', level=logging.INFO, format='%(asctime)s %(message)s', datefmt='%d/%m/%Y %I:%M:%S %p')
 	percentageFile=open("/usr/local/lib/chip-battery-status/percentage", "r")
 	isChargingFile=open("/usr/local/lib/chip-battery-status/charging", "r")
 	percentage=percentageFile.read()
